11b77c18f91991d3bbcbba42954e857c83d95f34
Kane0068 <mustafacat31@gmail.com>
2025-01-09 15:47:36 +0300

    Fixed VerifyLens.tsx

--- verifylens-frontend/src/components/ui/VerifyLens.tsx
+++ verifylens-frontend/src/components/ui/VerifyLens.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, ChangeEvent, FormEvent } from 'react';
 import { Upload, AlertCircle, CheckCircle2 } from 'lucide-react';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
